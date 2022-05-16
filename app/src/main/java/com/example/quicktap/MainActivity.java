737cfd029d75ca888ee7c12df6f3eb7c9bfc860f
Kemmotsu-Harutaro <Harutaro.Kemmotsu@gmail.com>
2022-05-17 00:03:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/quicktap/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.quicktap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //プライベート変数の定義
+    private TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+
+        ((Button)findViewById(R.id.start)).setOnClickListener(this);
+        ((Button)findViewById(R.id.score)).setOnClickListener(this);
+    }
+
+    @Override //onClickメソッド実装
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.start):
+                title.setText("Hello World");
+                break;
+            case (R.id.score):
+                break;
+        }
+
+    }
+}
