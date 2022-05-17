6d1d3737334249a91d610b13ddfe52e4161286a6
ctc2022 <yodo.kawai123@gmail.com>
2022-05-17 15:07:22 +0900

    河合

--- app/src/main/java/com/example/quicktap/MainActivity.java
+++ app/src/main/java/com/example/quicktap/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case (R.id.start):
-                title.setText("Hello World");
+                title.setText("Hello ");
                 break;
             case (R.id.score):
                 break;
