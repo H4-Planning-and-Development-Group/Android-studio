d4cc9bfc3306ea9dd3d5b2add043516aaf9b3909
ctc2022 <manami.ono3232@gmail.com>
2022-05-17 15:09:55 +0900

    大野

--- app/src/main/java/com/example/quicktap/MainActivity.java
+++ app/src/main/java/com/example/quicktap/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case (R.id.start):
-                title.setText("おはよう");
+                title.setText("おはようです");
                 break;
             case (R.id.score):
                 break;
